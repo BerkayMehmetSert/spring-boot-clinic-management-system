84101baa22b696fccf55168632e323cfa2e7fd6e
Berkay <berkaymehmetsert@gmail.com>
2023-01-04 14:46:02 +0300

    Create project

---
+++ src/main/java/com/bms/clinicmanagementsystem/ClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bms.clinicmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bms/clinicmanagementsystem/ClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.clinicmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
