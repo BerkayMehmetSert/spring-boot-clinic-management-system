e63514faaea9f7c5a288d2f4850dbb0d580b3a36
Berkay <berkaymehmetsert@gmail.com>
2023-01-04 17:57:29 +0300

    Update repository

--- src/main/java/com/bms/clinicmanagementsystem/repository/EmployeeRepository.java
+++ src/main/java/com/bms/clinicmanagementsystem/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.bms.clinicmanagementsystem.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    boolean existsByUsername(String username);
 }
--- src/main/java/com/bms/clinicmanagementsystem/repository/RoleRepository.java
+++ src/main/java/com/bms/clinicmanagementsystem/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import com.bms.clinicmanagementsystem.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
+    boolean existsByNameIgnoreCase(String name);
 }
